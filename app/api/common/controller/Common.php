6de26bf238a0f7d9472c7afc1eb9194d612636ca
yanghuan <1354717176@qq.com>
2017-03-20 21:22:06 +0800

    报名html+js验证

---
+++ app/api/common/controller/Common.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\api\common\controller;
+
+use think\Controller;
+
+/**
+ * 前台-公共类
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/20
+ * Time: 19:45
+ */
+class Common extends Controller
+{
+    protected function _initialize()
+    {
+
+    }
+}
