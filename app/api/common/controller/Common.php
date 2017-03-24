6ac2927c5bb9305698d28766aa552e3203c601b1
yanghuan <1354717176@qq.com>
2017-03-24 21:46:29 +0800

    微信和本地通信搭建

--- app/api/common/controller/Common.php
+++ app/api/common/controller/Common.php
@@ -2,6 +2,7 @@
 namespace app\api\common\controller;
 
 use think\Controller;
+use think\Request;
 
 /**
  * 前台-公共类
@@ -12,8 +13,10 @@ use think\Controller;
  */
 class Common extends Controller
 {
+    protected $request = '';
+
     protected function _initialize()
     {
-
+        $this->request = Request::instance();
     }
 }
---
+++ app/desk/weixin/controller/Index.php
@@ -0,0 +1,65 @@
+<?php
+namespace app\desk\weixin\controller;
+
+use app\api\common\controller\Common;
+use think\Cache;
+
+/**
+ * 微信类
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/24
+ * Time: 21:17
+ */
+class Index extends Common
+{
+    /**
+     * 微信接口入口文件
+     * @author yanghuan
+     * @datetime 2017/3/24 21:45
+     * @return mixed
+     */
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 微信接入时，需用到的
+     * @author yanghuan
+     * @datetime 2017/3/24 21:40
+     */
+    private function checkWeixin()
+    {
+        $echostr = $this->request->get("echostr");
+        if ($this->checkSignature()) {
+            echo $echostr;
+            exit;
+        }
+    }
+
+    /**
+     * 微信接入时，验证获取微信请求过来的参数
+     * @author yanghuan
+     * @datetime 2017/3/24 21:40
+     * @return bool
+     */
+    private function checkSignature()
+    {
+        $signature = $this->request->get("signature");
+        $timestamp = $this->request->get("timestamp");
+        $nonce = $this->request->get("nonce");
+
+        $token = '1354717176';
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
